d692281729c5b2cbd35c864ea9222258b2408006
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-08-30 11:07:23 -0700

    removed forms from home

--- front-end/src/components/Home/index.tsx
+++ front-end/src/components/Home/index.tsx
@@ -1,13 +1,8 @@
-import SignupForm from "../SignupForm";
-import LoginForm from "../LoginForm";
 import './Home.css'
 
 const Home = () => {
   return (
-    <div className='HomeFormsCont'>
-      <LoginForm />
-      <SignupForm />
-    </div>
+    <div>Home</div>
   );
 };
 
